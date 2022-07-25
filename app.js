9b4bc7d2aa687c3c273b4ce55334d064241c1135
HeniMKH <108895187+HeniMKH@users.noreply.github.com>
2022-07-25 23:05:25 +0100

    sheep code

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
-var links;
+var count2;
 
-
-links = ['google', 'sky news', 'https://barclays.co.uk/', 'CYF', 'bbc'];
-while (!!links.length) {
-  if(--window.LoopTrap <= 0) throw "Infinite loop.";
-  let element_list = document.getElementById('list');
-  let new_li = document.createElement('li');
-  let new_a = document.createElement('a');
-  new_a.innerText = links.shift();
-  new_a.setAttribute("href", links.shift());
-
-  new_li.appendChild(new_a);
-
-  element_list.appendChild(new_li);
+function getNumberOrString(value) {
+  // Convert a string value to a number if possible
+  let number_value = Number(value);
+  if (Number.isNaN(number_value)) {
+    return value
+  } else {
+    return number_value
+  }
 }
+
+
+count2 = 0;
+count2 = 0;
+
+
+document.getElementById('button').addEventListener('click', (event) => {
+  if (getNumberOrString(document.getElementById('text').value) == 'sheep') {
+    count2 = (typeof count2 === 'number' ? count2 : 0) + 1;
+    let element_sheep_count = document.getElementById('sheep_count');
+    element_sheep_count.innerText = count2;
+  }
+  if (getNumberOrString(document.getElementById('text').value) != 'sheep') {
+    count2 = (typeof count2 === 'number' ? count2 : 0) + 1;
+    let element_0ther_count = document.getElementById('0ther_count');
+    element_0ther_count.innerText = count2;
+  }
+
+});
